c6d0fbb46454af904d28e26543e87f4e8c37ee27
RicTieri <tieri.riccardo@gmail.com>
2023-12-04 16:06:13 +0100

    added base for milestone

--- js/script.js
+++ js/script.js
@@ -3,8 +3,9 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-
-      },
+        newElement: {text: '', done: false},
+        list: [{text: 'a', done: false},{text: 'b', done: true},{text: 'c', done: false},],
+      }
     },
     methods:{
 
